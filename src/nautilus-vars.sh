cbc83983b62e5c01790536369b7b896291999f78
Marc MOREAU <moreau.marc.web@gmail.com>
2020-04-04 00:07:44 +0200

    component creation

--- src/nautilus-vars.sh
+++ src/nautilus-vars.sh
@@ -2,22 +2,41 @@
 
 tempFile=$(mktemp)
 
-echo "NAUTILUS_SCRIPT_SELECTED_FILE_PATHS:\n" > ${tempFile}
-echo ${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS} > ${tempFile}
 
-echo "\n" > ${tempFile}
+echo "\$0:" >> ${tempFile}
+echo ${0} >> ${tempFile}
 
-echo "NAUTILUS_SCRIPT_SELECTED_URIS:\n" > ${tempFile}
-echo ${NAUTILUS_SCRIPT_SELECTED_URIS} > ${tempFile}
+echo "" >> ${tempFile}
 
-echo "\n" > ${tempFile}
+dir=$(dirname "${0}")
+dir=$(realpath "${dir}")
+echo "dir:" >> ${tempFile}
+echo ${dir} >> ${tempFile}
 
-echo "NAUTILUS_SCRIPT_CURRENT_URI:\n" > ${tempFile}
-echo ${NAUTILUS_SCRIPT_CURRENT_URI} > ${tempFile}
+echo "" >> ${tempFile}
 
-echo "\n" > ${tempFile}
+echo "NAUTILUS_SCRIPT_SELECTED_FILE_PATHS:" >> ${tempFile}
+echo ${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS} >> ${tempFile}
 
-echo "NAUTILUS_SCRIPT_WINDOW_GEOMETRY:\n" > ${tempFile}
-echo ${NAUTILUS_SCRIPT_WINDOW_GEOMETRY} > ${tempFile}
+
+for file in ${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS}
+   do
+     echo ${file} >> ${tempFile}
+done
+
+echo "" >> ${tempFile}
+
+echo "NAUTILUS_SCRIPT_SELECTED_URIS:" >> ${tempFile}
+echo ${NAUTILUS_SCRIPT_SELECTED_URIS} >> ${tempFile}
+
+echo "" >> ${tempFile}
+
+echo "NAUTILUS_SCRIPT_CURRENT_URI:" >> ${tempFile}
+echo ${NAUTILUS_SCRIPT_CURRENT_URI} >> ${tempFile}
+
+echo "" >> ${tempFile}
+
+echo "NAUTILUS_SCRIPT_WINDOW_GEOMETRY:" >> ${tempFile}
+echo ${NAUTILUS_SCRIPT_WINDOW_GEOMETRY} >> ${tempFile}
 
 gedit ${tempFile}

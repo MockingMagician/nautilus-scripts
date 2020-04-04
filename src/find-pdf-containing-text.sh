b645dd01c7ad07944e7dcc4c26751cf5d9d3c8db
Marc MOREAU <moreau.marc.web@gmail.com>
2020-04-05 00:06:29 +0200

    find text in pdf

--- src/find-pdf-containing-text.sh
+++ src/find-pdf-containing-text.sh
@@ -3,18 +3,24 @@
 pwd=$(pwd)
 tempFile=$(mktemp)
 term=$(zenity --entry --text="Terme à rechercher")
+found=$(find "${pwd}" -name '*.pdf')
 
-for found in $(find ${pwd} -name '*.pdf')
+while IFS= read -r file
     do
-    text=$(pdftotext ${found} -)
-    grep=$(echo "${text}" | grep -i --color ${term})
+    text=$(pdftotext "${file}" -)
+    grep=$(echo "${text}" | grep -i --color "${term}")
     if [[ -z "${grep}" ]]
         then continue
     fi
-    echo -e "Found matches in file: $(realpath ${found})\n" >> ${tempFile}
+    echo -e "Found matches in file: "$(realpath "${file}")"\n" >> ${tempFile}
     echo "Matches:" >> ${tempFile}
-    echo ${grep} >> ${tempFile}
+    while IFS= read -r match
+    do
+        echo "=> "${match} >> ${tempFile}
+    done < <(printf '%s\n' "${grep}")
     echo -e "\n*** *** ***\n" >> ${tempFile}
-done
+done < <(printf '%s\n' "${found}")
 
 gedit ${tempFile}
+
+sh
